670845e1c919458eb0317e0c576d5fc219af9407
webik150 <webik150@xenonlabs.eu>
2022-02-02 21:29:45 +0100

    add comments to PacketRefill

--- src/main/java/invtweaks/network/packets/ITPacketRefill.java
+++ src/main/java/invtweaks/network/packets/ITPacketRefill.java
@@ -15,10 +15,18 @@ import org.jetbrains.annotations.NotNull;
  * Handles refilling of hotbar.
  */
 public class ITPacketRefill implements ITPacket {
-    public int targetSlot;
-    public int refillFromSlot;
-    public int playerId;
 
+    protected int targetSlot;
+    protected int refillFromSlot;
+    protected int playerId;
+
+    /**
+     * Creates a new Refill packet to send to server. Unlike sorting, this uses Inventory slot IDs.
+     * That means 0-8 for hotbar, 9-35 for inventory, starting from TOP-LEFT.
+     * @param targetSlot The slot we want to fill.
+     * @param refillFromSlot The slot we want to take the item from.
+     * @param playerId Player that triggered this refill.
+     */
     public ITPacketRefill(int targetSlot, int refillFromSlot, int playerId) {
         this.targetSlot = targetSlot;
         this.refillFromSlot = refillFromSlot;
@@ -36,18 +44,26 @@ public class ITPacketRefill implements ITPacket {
         buffer.writeInt(playerId);
     }
 
+    /**
+     * Handles received packet.
+     * @param context
+     */
     @Override
     public void handle(NetworkEvent.Context context) {
         if(context.getDirection() == NetworkDirection.PLAY_TO_SERVER) {
             ServerPlayer player = context.getSender();
+            // Spectators don't really have an inventory so we don't handle those. If for any reason player IDs don't match (shouldn't happen), we also don't do anything.
             if(!player.isSpectator() && player.getId() == playerId) {
                 var inv = player.getInventory();
+                // Check that we have anything to refill from
                 if(!inv.getItem(refillFromSlot).isEmpty()){
                     ItemStack replacedItem = ItemStack.EMPTY;
+                    // Check whether we're filling an empty slot, or replacing a tool.
                     if(!inv.getItem(targetSlot).isEmpty()){
                         replacedItem = inv.getItem(targetSlot);
                     }
 
+                    // Replace with a copy to avoid duplication (I guess? I've seen people do it lol)
                     inv.setItem(targetSlot, player.containerMenu.getSlot(refillFromSlot).getItem().copy());
                     inv.setItem(refillFromSlot, replacedItem);
 
